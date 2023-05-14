3f45ce936ba303d647ebe0fdbeb95d6388a8c8d2
Gary Clarke <garyclarke1974@gmail.com>
2023-05-14 13:27:05 +0100

    Remove USD, GBP, and duplication in tests

--- tests/MultiCurrencyTest.php
+++ tests/MultiCurrencyTest.php
@@ -1,9 +1,6 @@
 <?php // /tests/MultiCurrencyTest.php
 
-
-use App\GBP;
 use App\Money;
-use App\USD;
 use PHPUnit\Framework\TestCase;
 
 class MultiCurrencyTest extends TestCase
@@ -19,16 +16,7 @@ class MultiCurrencyTest extends TestCase
     {
         $this->assertTrue((Money::gbp(5))->equals(Money::gbp(5)));
         $this->assertFalse((Money::gbp(5))->equals(Money::gbp(6)));
-        $this->assertTrue((Money::usd(5))->equals(Money::usd(5)));
-        $this->assertFalse((Money::usd(5))->equals(Money::usd(6)));
         $this->assertFalse((Money::usd(5))->equals(Money::gbp(5)));
-    }
-
-    public function testUSDMultiplication(): void
-    {
-        $five = Money::usd(5);
-        $this->assertEquals(Money::usd(10), $five->times(2));
-        $this->assertEquals(Money::usd(15), $five->times(3));
     }
 
     public function testCurrency(): void
